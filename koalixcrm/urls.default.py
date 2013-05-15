d0a6cae16c2c76a72c0f0047617f05780f951710
igorn <igorn@freshlimestudio.com>
2013-05-15 18:42:57 +0300

    fix url for media-files

--- koalixcrm/urls.default.py
+++ koalixcrm/urls.default.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from filebrowser.sites import site
@@ -12,4 +13,4 @@ urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/admin'}),
     (r'^grappelli/', include('grappelli.urls')),
     (r'^admin/filebrowser/', include(site.urls)),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=os.path.dirname(settings.MEDIA_ROOT))
